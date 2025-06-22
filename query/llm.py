1ecd0d6641f309b161064fb2976591f54def25b5
zolotykh907 <i.zolotykh@g.nsu.ru>
2025-06-23 00:57:34 +0700

    new langchain

--- query/llm.py
+++ query/llm.py
@@ -1,33 +1,55 @@
-from langchain.llms import Ollama
-from langchain.chains import LLMChain
-from langchain.prompts import PromptTemplate
+# from langchain.llms import Ollama
+# from langchain.chains import LLMChain
+# from langchain.prompts import PromptTemplate
+# import os
+
+# class LLMResponder:
+#     def __init__(self, config):
+#         self.model_name = config.llm
+#         self.llm = Ollama(
+#             model=self.model_name,
+#             base_url=os.getenv("OLLAMA_HOST", "http://localhost:11434")
+#         )
+#         # self.prompt_template = PromptTemplate(
+#         #     input_variables=["question", "context"],
+#         #     template=(
+#         #         "Ты — интеллектуальный помощник, использующий информацию из базы знаний, чтобы точно ответить на вопрос.\n\n"
+#         #         "Вопрос: {question}\n\n"
+#         #         "Контекст:\n{context}\n\n"
+#         #         "Если точного ответа нет, напиши что не нашел информацию. Ответ пиши всегда на русском языке.\n\n"
+#         #         "Если нашел - то процитируй или перефразируй, оставляя только самое важное для ответа на вопрос"
+#         #     )
+#         # )
+#         self.prompt_template = PromptTemplate(
+#             input_variables=["question", "context"],
+#             template = config.prompt_template
+#         )
+
+#         self.chain = LLMChain(prompt=self.prompt_template, llm=self.llm)
+
+#     def generate_answer(self, question: str, texts: list[str]) -> str:
+#         context = "\n\n".join(texts)
+#         response = self.chain.run(question=question, context=context)
+#         return response.strip()
+
 import os
+
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama.llms import OllamaLLM
+
 
 class LLMResponder:
     def __init__(self, config):
         self.model_name = config.llm
-        self.llm = Ollama(
-            model=self.model_name,
-            base_url=os.getenv("OLLAMA_HOST", "http://localhost:11434")
-        )
-        # self.prompt_template = PromptTemplate(
-        #     input_variables=["question", "context"],
-        #     template=(
-        #         "Ты — интеллектуальный помощник, использующий информацию из базы знаний, чтобы точно ответить на вопрос.\n\n"
-        #         "Вопрос: {question}\n\n"
-        #         "Контекст:\n{context}\n\n"
-        #         "Если точного ответа нет, напиши что не нашел информацию. Ответ пиши всегда на русском языке.\n\n"
-        #         "Если нашел - то процитируй или перефразируй, оставляя только самое важное для ответа на вопрос"
-        #     )
-        # )
-        self.prompt_template = PromptTemplate(
-            input_variables=["question", "context"],
-            template = config.prompt_template
-        )
+        self.prompt_template = ChatPromptTemplate.from_template(config.prompt_template)
+        self.llm = OllamaLLM(model="llama3",
+                             base_url=os.getenv("OLLAMA_HOST", "http://localhost:11434")
+                             )
 
-        self.chain = LLMChain(prompt=self.prompt_template, llm=self.llm)
+        self.chain = self.prompt_template | self.llm
 
-    def generate_answer(self, question: str, texts: list[str]) -> str:
-        context = "\n\n".join(texts)
-        response = self.chain.run(question=question, context=context)
-        return response.strip()
+    def generate_answer(self, question, texts):
+        context = '\n'.join(texts)
+        response = self.chain.invoke(question=question, context=context)
+
+        return response
