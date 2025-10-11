3e376c620037996e2574e1d09d3fa56771ddf00d
zolotykh907 <i.zolotykh@g.nsu.ru>
2025-10-12 00:02:59 +0700

    added axios and buttons for switch fetch method

--- documentation/react-app/src/sections/Posts.js
+++ documentation/react-app/src/sections/Posts.js
@@ -9,21 +9,47 @@ function Posts() {
     const [countPosts, setCountPosts] = useState(5)
     const [useAxios, setUseAxios] = useState(false); // false = fetch, true = axios
 
-    useEffect(() => {
-        const fetchPosts = async () => {
+    const fetchPosts = async () => {
                 const response = await fetch(POSTS_URL)
                 const data = await response.json()
                 setPosts(data)
         }
-        fetchPosts()
-    }, [])
+
+    const axiosPosts = async () => {
+        const response = await axios.get(POSTS_URL)
+        setPosts(response.data)
+    }
+    useEffect(() => {
+        if (useAxios) {
+            axiosPosts()
+        }
+        else {
+            fetchPosts()
+        }
+
+    }, [useAxios])
 
     const visiblePosts = useMemo(() => posts.slice(0, countPosts), [posts, countPosts]);
 
     return (
         <section>
             <h2> Посты</h2>
-            <div className='posts-meta'>
+            <div className='posts-config'>
+                <div className="method-toggle">
+                    <button
+                        className={`method-btn ${!useAxios ? 'active' : ''}`}
+                        onClick={() => setUseAxios(false)}
+                    >
+                        Fetch
+                    </button>
+
+                    <button
+                        className={`method-btn ${useAxios ? 'active' : ''}`}
+                        onClick={() => setUseAxios(true)}
+                    >
+                        Axios
+                    </button>
+                </div>
                 <Slider value={countPosts} onChange={setCountPosts} />
                 <p className='slidebar-value'>{countPosts}</p>
             </div>
