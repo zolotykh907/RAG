3c1cf3c8018c611b64c39e1955dae96bf33c38ba
zolotykh907 <i.zolotykh@g.nsu.ru>
2025-12-15 22:17:54 +0700

    chore: remove documentation folder

--- documentation/react-app/craco.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  webpack: {
-    configure: (webpackConfig) => {
-      // Ignore source map warnings from node_modules
-      webpackConfig.ignoreWarnings = [/Failed to parse source map/];
-      return webpackConfig;
-    },
-  },
-};
