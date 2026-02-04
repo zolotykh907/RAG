24d8742c73068136e1dd49c72603ea86915bd945
zolotykh907 <i.zolotykh@g.nsu.ru>
2026-02-04 10:10:03 +0700

    fix: fix bugs

--- query/query.py
+++ query/query.py
@@ -2,12 +2,10 @@ import json
 import os
 from pathlib import Path
 
-import faiss
 from sentence_transformers import SentenceTransformer
 from tqdm import tqdm
 
 import sys
-import os
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'shared'))
 from logs import setup_logging
 
